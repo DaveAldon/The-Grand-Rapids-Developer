f999acffcf54f9035d3143bff1a4683cc8397f1d
David Crawford <crawford@mail.gvsu.edu>
2022-12-15 21:24:20 -0500

    Fixed params

--- components/Links.tsx
+++ components/Links.tsx
@@ -45,10 +45,10 @@ const Link = ({ title, description, imageSrc, link }) => (
               className="w-4 h-4 ml-2"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
               fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
