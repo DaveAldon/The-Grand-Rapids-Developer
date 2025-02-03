a072f6ea7a25de252191dfd068fa405ea397bc41
David Crawford <crawford@mail.gvsu.edu>
2025-02-03 16:06:04 -0500

    Added posts

--- components/periodicTable/tableElement.tsx
+++ components/periodicTable/tableElement.tsx
@@ -85,19 +85,19 @@ export const ArticleAvailableIcon = () => {
       width="12"
       height="12"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <g
           id="Page-1"
           stroke="none"
-          stroke-width="1"
+          strokeWidth="1"
           fill="none"
-          fill-rule="evenodd"
+          fillRule="evenodd"
         >
           <g
             id="Icon-Set-Filled"
